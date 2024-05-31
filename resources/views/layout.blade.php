245056474e081da1f364e03fde10acd753b104fc
enrtorgil <enrtorgil@alu.edu.gva.es>
2024-05-31 18:43:31 +0200

    fix -> favicon redimensionado y cambiado a .png

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,7 @@
     <title>
         @yield('title')
     </title>
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
